026a41eb29511b4a3554a753a731c0cd91272530
cpd67 <codemonkey941@gmail.com>
2020-04-15 20:51:37 -0400

    Add comment giving source.

--- db/migrate/20200415221200_add_password_to_user.rb
+++ db/migrate/20200415221200_add_password_to_user.rb
@@ -1,3 +1,4 @@
+# https://medium.com/@tpstar/password-digest-column-in-user-migration-table-871ff9120a5
 class AddPasswordToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :password_digest, :string
