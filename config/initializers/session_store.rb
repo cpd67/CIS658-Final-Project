e432ef16de0e55c010c4f9ebd0fb533093fc9ac6
Chris Dilley <dilleyc@mail.gvsu.edu>
2020-04-20 20:12:59 -0400

    Update session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # https://medium.com/how-i-get-it/react-with-rails-user-authentication-8977e98762f2
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_money-trail', domain: 'localhost'
+    Rails.application.config.session_store :cookie_store, key: '_money-trail', domain: 'https://money-trail-app.herokuapp.com/'
 else
-    Rails.application.config.session_store :cookie_store, key: '_money-trail'
+    Rails.application.config.session_store :cookie_store, key: '_money-trail', domain: 'localhost'
 end
