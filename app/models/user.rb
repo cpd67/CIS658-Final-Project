f43a890b6aca4a402d4f94fc338d489ca2b76e4f
Chris Dilley <dilleyc@mail.gvsu.edu>
2020-04-22 19:03:45 -0400

    Add source.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_many :expenses
     has_many :categories
 
+    # https://guides.rubyonrails.org/active_record_validations.html#message
     validates :username, presence: true, uniqueness: { message: "Username is already taken" }
     has_secure_password
 end
